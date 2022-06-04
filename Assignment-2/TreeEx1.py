aee92e262f6d7b367f4fafa5b5ad2b71aef27908
Wassay123 <80175602+Wassay123@users.noreply.github.com>
2022-06-03 20:44:01 -0400

    Update TreeEx1.py
    Updated code to reflect more edge cases

--- Assignment-2/TreeEx1.py
+++ Assignment-2/TreeEx1.py
@@ -1,27 +1,30 @@
 class Node:
-    def __init__(self, value, left = None, right = None):
+    def __init__(self, value, children = []):
         self.value = value
-        self.left = left
-        self.right = right
+        self.children = children
 
 class Tree:
     def __init__(self, root):
-        self.root = Node(root)
+        self.root = root
 
 #EX 1
 def printTree(root):
     if root:
         print(root.value, end=" ")
-        printTree(root.left)
-        printTree(root.right)
+        if root.children:
+            for each in root.children:
+                printTree(each)
+
 
 
 if __name__ == "__main__":
-    tree = Tree(1)
-    tree.root.left = Node(7)
-    tree.root.right = Node(17)
-    tree.root.right.left = Node(6)
-    tree.root.right.right = Node(3)
+    #test case for a binary tree
+    node3 = Node(3)
+    node6 = Node(6)
+    node17 = Node(17, [node6, node3])
+    node7 = Node(7)
+    node1 = Node(1, [node7, node17])
+    tree = Tree(node1)
 
     #     1
     #    /  \
@@ -31,20 +34,39 @@ if __name__ == "__main__":
     
     printTree(tree.root)
     #successfully prints 1 7 17 6 3 
+    print("")
 
-    tree = Tree(1)
-    tree.root.left = Node(2)
-    tree.root.right = Node(5)
-    tree.root.left.left = Node(3)
-    tree.root.left.right = Node(4)
-    tree.root.right.left = Node(6)
-    tree.root.right.right = Node(7)
+    #test case for a non-binary tree
+    node10 = Node(10)
+    node9 = Node(9)
+    node8 = Node(8)
+    node7 = Node(7)
+    node6 = Node(6)
+    node5 = Node(5)
+    node3 = Node(3)
+    node2 = Node(2, [node5, node6, node7])
+    node4 = Node(4, [node8, node9, node10])
+    node1 = Node(1, [node2, node3, node4])
+    tree2 = Tree(node1)
 
-    #       1
-    #     /   \
-    #    2     5
-    #   / \   / \
-    #  3   4 6   7
+    #           1
+    #         / | \
+    #       /   |   \
+    #      2    3    4
+    #    / | \     / | \
+    #   /  |  \   /  |  \
+    #  5   6   7  8  9   10
 
-    printTree(tree.root)
-    #successfully prints 1 2 3 4 5 6 7 
+    printTree(tree2.root)
+    #successfully prints 1 2 5 6 7 3 4 8 9 10 
+    print("")
+
+    #test case for a tree of 1 node
+    node1 = Node(1)
+    tree3 = Tree(node1)
+
+    # 1
+
+    printTree(tree3.root)
+    #successfully ptins 1 
+
