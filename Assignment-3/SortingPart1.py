b4dcbc7631c43e243c28602aef4adac998931fc8
Wassay123 <80175602+Wassay123@users.noreply.github.com>
2022-08-12 19:35:25 -0400

    Update SortingPart1.py
    renamed some variables for improved readability

--- Assignment-3/SortingPart1.py
+++ Assignment-3/SortingPart1.py
@@ -1,30 +1,30 @@
 #When partitioning our list we want to choose a pivot in arr, which will be the last element of arr to keep it simple
 
-def partition(arr, end, list1_end, list2_start):
+def partition(arr, end, half1_end, half2_start):
 
     pivot = arr[end]
     #[5, 10, 5, 20, 5, 5, 10]
     #pivot = 10
-    #list1_end = [0]
-    #list2_start = [0]
+    #half1_end = [0]
+    #half2_start = [0]
     #end = 6
 
     #Continue while mid is not greater than end
-    while list2_start[0] <= end:
+    while half2_start[0] <= end:
 
         #If starting element is less than pivot then swap
-        if arr[list2_start[0]] < pivot:
-            arr[list2_start[0]], arr[list1_end[0]] = arr[list1_end[0]], arr[list2_start[0]]
-            list2_start[0] += 1
-            list1_end[0] += 1
+        if arr[half2_start[0]] < pivot:
+            arr[half2_start[0]], arr[half1_end[0]] = arr[half1_end[0]], arr[half2_start[0]]
+            half2_start[0] += 1
+            half1_end[0] += 1
              
         #If ending element is greater than pivot then swap
-        elif arr[list2_start[0]] > pivot:
-            arr[list2_start[0]], arr[end] = arr[end], arr[list2_start[0]]
+        elif arr[half2_start[0]] > pivot:
+            arr[half2_start[0]], arr[end] = arr[end], arr[half2_start[0]]
             end -= 1
              
-        elif arr[list2_start[0]] == pivot:
-            list2_start[0] += 1
+        elif arr[half2_start[0]] == pivot:
+            half2_start[0] += 1
 
 
 def partitionSort(arr, start, end):
@@ -42,15 +42,15 @@ def partitionSort(arr, start, end):
             return
 
     #Recursive Case of 3+ element arr
-    list1_end = [start]
-    list2_start = [start]
+    half1_end = [start]
+    half2_start = [start]
 
     #First partition our list
-    partition(arr, end, list1_end, list2_start) 
+    partition(arr, end, half1_end, half2_start) 
     
     #Now recursively use quicksort on our arr
-    partitionSort(arr, start, list1_end[0] - 1)
-    partitionSort(arr, list2_start[0], end)
+    partitionSort(arr, start, half1_end[0] - 1)
+    partitionSort(arr, half2_start[0], end)
 
 
 
