fb6ff0af9020c40c2a48d6109d4908fa66122f22
Wassay123 <80175602+Wassay123@users.noreply.github.com>
2022-08-13 01:29:35 -0400

    Update SortingPart2.py
    Completed portion of code to create k sorted lists depending on wanted chunk load in size as well as overall size of the massive array (for testing purposes I set it to 1000 to ensure code works correctly

--- Assignment-3/SortingPart2.py
+++ Assignment-3/SortingPart2.py
@@ -1,1 +1,48 @@
+import random
+import numpy as np
 
+def createInputFile(fileName, size):
+#Code used to create the random list csv file
+    csvfile = open(fileName, "w")
+
+    for i in range(size - 1):
+        int = random.randint(1, 1000)
+        csvfile.write(str(int))
+        csvfile.write(",\n")
+
+    int = random.randint(1, 1000)
+    csvfile.write(str(int))
+
+    csvfile.close()
+
+#Chunk size is the amount of numbers we want to load into memory
+#total_size and the size paramter used in the create InputFile must be the same number
+def createSortedFiles(InputFile, total_size, chunk_size):
+    with open(InputFile, "r") as file:
+        arr = []
+        files = 10
+
+        for i in range(files):
+
+            #Read 100 numbers at a time from the 1,000,000 element list and add them to a temp arr
+            for j in range(chunk_size):
+                num = next(file).strip()
+                num = num.replace(",", "")
+                arr.append(int(num))
+
+            arr.sort()
+            sorted_list = np.array(arr)
+            arr = []
+
+            filename = "sorted_arrs" + str(i) + ".csv"
+
+            #creates 100 files each containing a sorted list of the 100 read
+            with open(filename, "w") as arr_file:
+                np.savetxt(arr_file, sorted_list)
+
+
+    
+if __name__ == "__main__":
+    arraySize = 1000
+    createInputFile("RandomList.csv", arraySize)
+    createSortedFiles("RandomList.csv", arraySize, 100)
